20915d081e7399653ae18a9386ebcc4698ada00a
fredcallaway <fredc@llaway.com>
2017-08-14 19:27:46 -0400

    adding a bunch of python modeling

---
+++ python/mouselab.py
@@ -0,0 +1,329 @@
+from collections import namedtuple, defaultdict, deque, Counter
+import numpy as np
+import gym
+from toolz import memoize, curry
+from gym import spaces
+import itertools as it
+from distributions import *
+
+class MouselabEnv(gym.Env):
+    """MetaMDP for a tree with a discrete unobserved reward function."""
+    metadata = {'render.modes': ['human', 'array']}
+    term_state = '__term_state__'
+    def __init__(self, branch=2, height=2, reward=None, cost=0, ground_truth=None):
+        self.branch = branch
+        if hasattr(self.branch, '__len__'):
+            self.height = len(self.branch)
+        else:
+            self.height = height
+            self.branch = [self.branch] * self.height
+
+        self.cost = - abs(cost)
+        self.reward = reward if reward is not None else Normal(1, 1)
+        self.ground_truth = np.array(ground_truth)
+
+        self.tree = self._build_tree()
+        self.init = (0,) + (self.reward,) * (len(self.tree) - 1)
+        self.term_action = len(self.tree)
+        self.reset()
+
+        self.action_space = spaces.Discrete(len(self.tree) + 1)
+        self.observation_space = spaces.Box(-np.inf, np.inf, shape=len(self.tree))
+        self.subtrees = self._get_subtrees()
+
+    def _reset(self):
+        self._state = self.init
+        return self.features(self._state)
+
+    def _step(self, action):
+        if self._state is self.term_state:
+            assert 0, 'state is terminal'
+            # return None, 0, True, {}
+        if action == self.term_action:
+            # self._state = self.term_state
+            if self.ground_truth is not None:
+                path = list(self.best_path())
+                reward = self.ground_truth[path].sum()
+            else:
+                reward = self.term_reward().sample()
+            done = True
+        elif self._state[action] is not self.reward:  # already observed
+            reward = 0
+            done = False
+        else:  # observe a new node
+            self._state = self._observe(action)
+            reward = self.cost
+            done = False
+        return self.features(self._state), reward, done, {}
+
+    def _observe(self, action):
+        if self.ground_truth is not None:
+            result = self.ground_truth[action]
+        else:
+            result = self._state[action].sample()
+        s = list(self._state)
+        s[action] = result
+        return tuple(s)
+
+    def actions(self, state):
+        """Yields actions that can be taken in the given state.
+
+        Actions include observing the value of each unobserved node and terminating.
+        """
+        if state is self.term_state:
+            return
+        for i, v in enumerate(state):
+            if v is self.reward:
+                yield i
+        yield self.term_action
+
+    def results(self, state, action):
+        """Returns a list of possible results of taking action in state.
+
+        Each outcome is (probability, next_state, reward).
+        """
+        if action == self.term_action:
+            # R = self.term_reward()
+            # S1 = Categorical([self.term_state])
+            # return cross(S1, R)
+            yield (1, self.term_state, self.expected_term_reward(state))
+        else:
+            for r, p in state[action]:
+                s1 = list(state)
+                s1[action] = r
+                yield (p, tuple(s1), self.cost)
+
+    def features(self, state=None):
+        state = state if state is not None else self._state
+        return state
+
+        # if state is None:
+        #     return np.full(len(self.tree), np.nan)
+        # # Is each node observed?
+        # return np.array([1. if hasattr(x, 'sample') else 0.
+        #                  for x in state])
+    
+    def action_features(self, action, state=None):
+        state = state if state is not None else self._state
+        assert state is not None
+
+        # if action == self.term_action:
+        #     tr_mu, tr_sigma = norm.fit(self.term_reward.sample(10000))
+        #     return np.r_[0, 0, 0, 0, 0, tr_mu, tr_sigma]
+        # nq_mu, nq_sigma = norm.fit(self.node_quality(action).sample(10000))
+        # nqpi_mu, nqpi_sigma = norm.fit(self.node_quality(action).sample(10000))
+        # return np.r_[1, nq_mu, nq_sigma, nqpi_mu, nqpi_sigma, 0, 0]
+
+        if action == self.term_action:
+            return np.array([
+                0,
+                0,
+                0,
+                0
+            ])
+
+        return np.array([
+            self.cost,
+            self.myopic_voc(action, state),
+            self.vpi_action(action, state),
+            self.vpi(state),
+        ])
+
+    @memoize
+    def subtree(self, node):
+        def gen(n):
+            yield n
+            for n1 in self.tree[n]:
+                yield from gen(n1)
+        return list(gen(node))
+
+    def term_reward(self, state=None):
+        state = state if state is not None else self._state
+        assert state is not None
+        return self.node_value(0, state)
+
+    def best_path(self, state=None):
+        state = state if state is not None else self._state
+        n = 0
+        while self.tree[n]:
+            n = max(self.tree[n],
+                    key=lambda n1: self.node_quality(n1, state).expectation())
+            yield n
+
+    @memoize
+    def expected_term_reward(self, state):
+        return self.term_reward(state).expectation()
+
+    def node_value(self, node, state=None):
+        """A distribution over total rewards after the given node."""
+        state = state if state is not None else self._state
+        return max((self.node_value(n1, state) + state[n1]
+                    for n1 in self.tree[node]), 
+                   default=PointMass(0), key=expectation)
+
+    def node_value_to(self, node, state=None):
+        """A distribution over rewards up to and including the given node."""
+        state = state if state is not None else self._state
+        start_value = PointMass(0)
+        return sum((state[n] for n in self.path_to(node)), start_value)
+
+    def node_quality(self, node, state=None):
+        """A distribution of total expected rewards if this node is visited."""
+        state = state if state is not None else self._state
+        return self.node_value_to(node, state) + self.node_value(node, state)
+
+    @memoize
+    def myopic_voc(self, action, state):
+        return (self.node_value_after_observe((action,), 0, state).expectation() -
+                self.expected_term_reward(state))
+
+    @memoize
+    def vpi_action(self, action, state):
+        obs = self._relevant_subtree(action)
+        return (self.node_value_after_observe(obs, 0, state).expectation() -
+                self.expected_term_reward(state))
+
+    @memoize
+    def vpi(self, state):
+        return (self.node_value_after_observe('all', 0, state).expectation() -
+                self.expected_term_reward(state))
+
+    def unclicked(self, state):
+        return sum(1 for x in state if hasattr(x, 'sample'))
+
+    @memoize
+    def _relevant_subtree(self, node):
+        trees = [self.subtree(n1) for n1 in self.tree[0]]
+        for t in trees:
+            if node in t:
+                return tuple(t)
+        print('Error', node)
+        assert False
+
+
+    @memoize
+    def node_value_after_observe(self, obs, node, state):
+        """A distribution over the expected value of node, after making an observation.
+        
+        obs can be a single node, a list of nodes, or 'all'
+        """
+        def r(n):
+            if obs == 'all' or n in obs:
+                return state[n]
+            else:
+                return expectation(state[n])
+
+        return cmax((self.node_value_after_observe(obs, n1, state) + r(n1)
+                     for n1 in self.tree[node]),
+                    default=PointMass(0))
+
+    def path_to(self, node, start=0):
+        path = [start]
+        if node == start:
+            return path
+        for _ in range(self.height + 1):
+            children = self.tree[path[-1]]
+            for i, child in enumerate(children):
+                if child == node:
+                    path.append(node)
+                    return path
+                if child > node:
+                    path.append(children[i-1])
+                    break
+            else:
+                path.append(child)
+        assert False
+
+    def all_paths(self, start=0):
+        def rec(path):
+            children = self.tree[path[-1]]
+            if children:
+                for child in children:
+                    yield from rec(path + [child])
+            else:
+                yield path
+
+        return rec([start])
+
+    def _get_subtrees(self):
+        slices = [0] * len(self.tree)
+        def get_end(n):
+            end = max((get_end(n1) for n1 in self.tree[n]), default=n+1)
+            slices[n] = slice(n, end)
+            return end
+        get_end(0)
+        return slices
+
+
+    def _build_tree(self):
+        # num_node = np.cumsum(self.branch).sum() + 1
+        def nodes_per_layer():
+            n = 1
+            yield n
+            for b in self.branch:
+                n *= b
+                yield n
+
+        num_node = sum(nodes_per_layer())
+        T = [[] for _ in range(num_node)]  # T[i] = [c1, c2, ...] or [] if i is terminal
+
+        ids = it.count(0)
+        def expand(i, d):
+            if d == self.height:
+                return
+            for _ in range(self.branch[d]):
+                next_i = next(ids)
+                T[i].append(next_i)
+                expand(next_i, d+1)
+
+        expand(next(ids), 0)
+        return T
+
+    def _render(self, mode='notebook', close=False):
+        from graphviz import Digraph
+        from IPython.display import display
+        import matplotlib as mpl
+        from matplotlib.colors import rgb2hex
+        if close:
+            return
+        
+        vmin = -2
+        vmax = 2
+        norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+        cmap = mpl.cm.get_cmap('RdYlGn')
+        colormap = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+        colormap.set_array(np.array([vmin, vmax]))
+
+        def color(val):
+            if val > 0:
+                return '#8EBF87'
+            else:
+                return '#F7BDC4'
+        
+        dot = Digraph()
+        for x, ys in enumerate(self.tree):
+            r = self._state[x]
+            observed = not hasattr(self._state[x], 'sample')
+            c = color(r) if observed else 'grey'
+            l = str(round(r, 2)) if observed else str(x)
+            dot.node(str(x), label=l, style='filled', color=c)
+            for y in ys:
+                dot.edge(str(x), str(y))
+        display(dot)
+
+
+@memoize
+def node_value_after_observe(self, obs, node, state):
+    """A distribution over the expected value of node, after making an observation.
+    
+    obs can be a single node, a list of nodes, or 'all'
+    """
+    def r(n):
+        if obs == 'all' or n in obs:
+            return state[n]
+        else:
+            return expectation(state[n])
+
+    return cmax((self.node_value_after_observe(obs, n1, state) + r(n1)
+                 for n1 in self.tree[node]),
+                default=PointMass(0))
---
+++ python/value_functions.py
@@ -0,0 +1,333 @@
+from collections import namedtuple, defaultdict, Counter, deque
+import itertools as it
+import numpy as np
+from abc import ABC, abstractmethod
+from utils import clear_screen, PriorityQueue
+import time
+np.set_printoptions(precision=3, linewidth=200)
+
+from tqdm import tqdm, trange, tnrange
+from copy import deepcopy
+# from keras.utils import to_categorical
+
+from agents import Component
+
+
+class ValueFunction(Component):
+    """Learns values."""
+    def __init__(self, learn_rate=.1, discount=1):
+        super().__init__()
+        self.discount = discount
+        self.learn_rate = learn_rate
+
+    def features(self, s):
+        if hasattr(self.env, 'nS'):
+            x = [0] * self.env.nS
+            x[s] = 1
+            return x
+        else:
+            return np.r_[1, s]
+
+    def predict(self, s):
+        return 0
+     
+
+class ActionValueFunction(ValueFunction):
+    """Values of state action pairs."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @abstractmethod
+    def predict(self, s):
+        x = self.features(s)
+        return self.model.predict(x)
+
+
+
+class MyQ(ActionValueFunction):
+    def __init__(self):
+        super().__init__()
+
+    def predict(self, s):
+        return 
+
+
+
+
+
+class NeuralQ(ActionValueFunction):
+    """Neural network approximation of Q function."""
+    def __init__(self, arg):
+        super().__init__()
+        self.model = self._build_model()
+
+    def _build_model(self):
+        actor = Sequential([
+            Dense(24, input_dim=self.state_size, activation='relu',
+                  kernel_initializer='he_uniform'),
+            # Dense(24, activation='relu',
+            #       kernel_initializer='he_uniform'),
+            Dense(self.n_action, activation='softmax',
+                  kernel_initializer='he_uniform')
+        ])
+        # actor.summary()
+        actor.compile(loss='mse',
+                      optimizer=Nadam(self.learn_rate))
+        return actor
+      
+
+class LinearQ(ActionValueFunction):
+    """Learns a linear Q function by SGD."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = None
+
+    def attach(self, agent):
+        super().attach(agent)
+        sx = len(self.features(self.env.reset()))
+        sy = self.agent.n_actions
+        shape = (sx, sy)
+        self.model = LinearSGD(shape, learn_rate=self.learn_rate)
+    
+    def experience(self, s0, a, s1, r, done):
+        x0, x1 = self.features(s0), self.features(s1)
+        target = self.model.predict(x1)
+        target[a] = r + self.discount * np.max(self.model.predict(x1))
+        self.model.update(x0, target)
+
+    def predict(self, s):
+        x = self.features(s)
+        return self.model.predict(x)
+
+# class LinearQ(ActionValueFunction):
+#     """Learns a linear Q function by SGD."""
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.model = None
+
+#     def attach(self, agent):
+#         super().attach(agent)
+#         sx = len(self.features(self.env.reset()))
+#         sy = self.agent.n_actions
+#         shape = (sx, sy)
+#         self.model = LinearSGD(shape, learn_rate=self.learn_rate)
+    
+#     def experience(self, s0, a, s1, r, done):
+#         x0, x1 = self.features(s0), self.features(s1)
+#         target = self.model.predict(x1)
+#         target[a] = r + self.discount * np.max(self.model.predict(x1))
+#         self.model.update(x0, target)
+
+#     def predict(self, s):
+#         x = self.features(s)
+#         return self.model.predict(x)
+
+
+class StateValueFunction(ValueFunction):
+    """Learns a linear V function by SGD."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+from models import BayesianRegression
+class BayesianRegressionV(StateValueFunction):
+    """Learns a linear V function by SGD."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = None
+
+    def attach(self, agent):
+        super().attach(agent)
+        self.model = BayesianRegression(np.zeros(self.state_size), sigma_w=10)
+
+    def predict(self, state, return_var=False):
+        return self.model.predict(state, return_var=return_var)
+
+    # def finish_episode(self, trace):
+    #     for batch in self.memory.batch(10):
+    #         states, rewards, returns = batch
+
+    #     self.model.update(batch['states'][:-1], batch['returns'])  # don't learn about final state
+    #     self.save('w', self.model.w.copy())
+    #     self.save('sigma_w', self.model.sigma_w.copy())
+
+
+# from models import BayesQ, BayesianRegression
+class BayesianRegressionQ(StateValueFunction):
+    """Learns a linear Q function by Bayesian regression."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = None
+
+    def attach(self, agent):
+        super().attach(agent)
+        # self.model = BayesQ(np.zeros((self.state_size, self.n_action)), sigma_w=10.)
+
+
+    def predict(self, state, return_var=False):
+        return 
+        # return self.model.predict(state, return_var=return_var)
+
+    def finish_episode(self, trace):
+        idx = self.memory.batch(1000)
+
+        states = []
+        actions = []
+        qs = []
+        for i in idx:
+            if self.memory.actions[i] is None:
+                continue  # can't update for final state
+            states.append(self.memory.states[i])
+            actions.append(self.memory.actions[i])
+            # value = self.model.predict([self.memory.states[i+1]]).max()
+            value = self.memory.returns[i+1]
+            qs.append(self.memory.rewards[i] + value)
+            
+        # actions = to_categorical(actions, num_classes=self.n_action)
+        # self.model.update(np.stack(states), actions, np.array(qs), 1)
+
+
+
+        # exps = self.memory.batch(1000)
+        # states, actions, next_states, returns, rewards = zip(*exps)
+        # target = self.model.predict(states)
+        
+
+
+        # for ep in self.memory.episodes(10):
+        #     print(batch)
+        #     self.model.update(batch['states']
+        #                       batch['returns'])
+
+        self.save('w', self.model.w.copy())
+        self.save('sigma_w', self.model.sigma_w.copy())
+
+
+class TDLambdaV(StateValueFunction):
+    """Learns a linear value function with TD lambda."""
+    def __init__(self, trace_decay=0, **kwargs):
+        super().__init__(**kwargs)
+        self.trace_decay = trace_decay
+        self.trace = None
+
+    def attach(self, agent):
+        self.agent = agent
+        shape = len(self.features(self.env.reset()))
+        self.trace = np.zeros(shape)
+        self.theta = np.zeros(shape)
+        self.theta_update = np.zeros(shape)
+
+    def start_episode(self, state):
+        self.theta = self.theta_update.copy()
+
+    def experience(self, s0, a, s1, r, done):
+        target = r + self.discount * self.predict(s1)
+        x = self.features(s0)
+        prediction = x @ self.theta
+        error = target - prediction
+        self.trace = self.trace_decay * self.trace + x
+        self.theta_update += self.learn_rate * error * self.trace
+        # self.theta_update *= self.decay
+
+    def predict(self, s):
+        x = self.features(s)
+        return x @ self.theta
+
+    def finish_episode(self, trace):
+        self.ep_trace['theta_v'] = self.theta.copy()
+
+
+class FixedV(StateValueFunction):
+    """User-specified value function."""
+    def __init__(self, theta):
+        super().__init__()
+        self.theta = np.array(theta)
+
+    def predict(self, s):
+        x = self.features(s)
+        return x @ self.theta
+
+    def finish_episode(self, trace):
+        self.ep_trace['theta_v'] = self.theta.copy()
+
+
+class FixedQ(StateValueFunction):
+    """User-specified action value function."""
+    def __init__(self, theta):
+        super().__init__()
+        self.theta = np.array(theta)
+
+    def predict(self, s):
+        
+        x = self.features(s)
+        return x @ self.theta
+
+    def finish_episode(self, trace):
+        self.ep_trace['theta_v'] = self.theta.copy()
+
+
+class LinearV(StateValueFunction):
+    """Learns a linear V function by SGD."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = None
+
+    def attach(self, agent):
+        super().attach(agent)
+        sx = len(self.features(self.env.reset()))
+        sy = 1
+        shape = (sx, sy)
+        self.model = LinearSGD(shape, learn_rate=self.learn_rate)
+
+    def experience(self, s0, a, s1, r, done):
+        x = self.features(s0)
+        # TODO: should we set target to 0 if done is True?
+        target = r + self.discount * self.predict(s1)
+        self.model.update(x, target)
+
+    def predict(self, s):
+        x = self.features(s)
+        return self.model.predict(x)[0]
+    
+    def finish_episode(self, trace):
+        self.ep_trace['theta_v'] = self.model.theta[:, 0].copy()
+
+
+# class MemV(StateValueFunction):
+#     """docstring for MemV"""
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.tbl = defaultdict(list)
+
+#     def finish_episode(self, trace):
+#         returns = concatv(reversed(np.cumsum(list(reversed(trace['rewards'])))), [0])
+#         for s, r in zip(trace['states'], returns):
+#             self.tbl[s].append(r)
+
+
+
+# from sklearn.linear_model import SGDRegressor
+# class MonteCarloV(ValueFunction):
+#     """Learns a linear value function with every-step Monte Carlo."""
+#     def __init__(self, env, **kwargs):
+#         super().__init__(env, **kwargs)
+#         self.model = SGDRegressor()
+
+#     def finish_episode(self, trace):
+#         X = np.array([self.features(s) for s in trace['states']])
+#         y = list(reversed(np.cumsum(list(reversed(trace['rewards'])))))
+#         y.append(0)  # value of final state
+#         self.model.partial_fit(X, y)
+
+#     def predict(self, s):
+#         if self.model.coef_ is not None:
+#             return self.model.predict(self.features(s).reshape(1, -1))[0]
+#         else:
+#             return 0
+
+#     @property
+#     def theta(self):
+#         return self.model.coef_
+
+
+
