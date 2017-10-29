e335d2cb4a28fc330a102f010ac8d545161a354f
fredcallaway <fredc@llaway.com>
2017-10-29 13:51:43 -0700

    maybe fixing the caching problem?

--- python/utils.py
+++ python/utils.py
@@ -1,6 +1,47 @@
 from IPython.display import clear_output
 import itertools as it
 import numpy as np
+import json
+
+from mouselab import MouselabEnv
+from distributions import Normal
+
+def make_env(cost, ground_truth=False, initial_states=None):
+    """Returns a MouselabEnv with branching [4,1,2].
+    
+    If `ground_truth` is True, the reward observed at a given node will be
+    constant across runs on this env. This reduces variance of the return."""
+    reward = Normal(0, 10).to_discrete(6)
+    env = MouselabEnv([4,1,2], reward=reward, cost=cost, initial_states=initial_states)
+    if ground_truth:
+        env.ground_truth = np.array([0, *reward.sample(len(env.tree) - 1)])
+    return env
+
+def make_envs(cost, n=100, ground_truth=None, initial_states=None):
+    # Note, ground_truth can be an int in which case it acts as a random seed.
+    if ground_truth is not None:
+        np.random.seed(ground_truth)
+        return [make_env(cost, True, initial_states) for _ in range(n)]
+    else:
+        return [make_env(cost, False, initial_states)] * n
+
+def read_state_actions():
+    with open('data/state_actions.json') as f:
+        data = json.load(f)
+    result = {}
+    for cost in data:
+        result[float(cost)] = r = {}
+        env = make_env(float(cost))
+        def parse_state(state):
+            return tuple(env.reward if x == '__' else float(x)
+                  for x in state)
+        def parse_action(action):
+            return env.term_action if action == '__TERM_ACTION__' else action
+        r['states'] = list(map(parse_state, data[cost]['states']))
+        r['actions'] = list(map(parse_action, data[cost]['actions']))
+    return result
+
+state_actions = read_state_actions()
 
 def join(*args, sep=' '):
     return sep.join(map(str, args))
