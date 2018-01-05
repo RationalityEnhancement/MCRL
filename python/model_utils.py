f41354603a2d18d974f027beb6d046ab71e73ee3
fredcallaway <fredc@llaway.com>
2018-01-05 10:40:22 -0800

    fix model_utils

--- python/model_utils.py
+++ python/model_utils.py
@@ -1,45 +1,9 @@
 from distributions import Normal
 from mouselab import MouselabEnv
-import numpy as np
 import json
 import skopt
 from policies import LiederPolicy
-from agents import Agent
 
-def clear_screen():
-    print(chr(27) + "[2J")
-    clear_output()
-
-import heapq
-class PriorityQueue(list):
-    def __init__(self, key, max_first=True):
-        self.key = key
-        self.inv = -1 if max_first else 1
-
-    def pop(self):
-        return heapq.heappop(self)[1]
-        
-    def push(self, item):
-        heapq.heappush(self, (self.inv * self.key(item), item))
-
-def make_env(cost, ground_truth=None, **kwargs):
-    """Returns a MouselabEnv with branching [4,1,2].
-    
-    If `ground_truth` is True, the reward observed at a given node will be
-    constant across runs on this env. This reduces variance of the return."""
-    reward = Normal(0, 10).to_discrete(6)
-    if ground_truth and (not hasattr(ground_truth, '__len__')):
-        ground_truth = np.array([0, *reward.sample(16)])
-    env = MouselabEnv([4,1,2], reward=reward, cost=cost, ground_truth=ground_truth, **kwargs)
-    return env
-
-def make_envs(cost, n=100, ground_truth=None, **kwargs):
-    # Note, ground_truth can be an int in which case it acts as a random seed.
-    if ground_truth is not None:
-        np.random.seed(ground_truth)
-        return [make_env(cost, True, **kwargs) for _ in range(n)]
-    else:
-        return [make_env(cost, None, **kwargs)] * n
 
 def filename(cost, note=''):
     c = round(float(cost), 5)
@@ -54,7 +18,11 @@ def read_bo_policy(cost, note=''):
     result = read_bo_result(cost, note)
     return LiederPolicy(result.specs['info']['theta'])
 
-ENV = make_env(0)
+def make_env(cost=1.25, ground_truth=None):
+    reward = Normal(0, 10).to_discrete(6)
+    return MouselabEnv.new_symmetric([4,1,2], reward, cost=cost, ground_truth=None)
+
+ENV = make_env()
 
 def parse_state(state):
     return tuple(ENV.reward if x == '__' else float(x)
